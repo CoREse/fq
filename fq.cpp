e4069348c593a57fa6bd91e91358ea8b2d6adf86
CRE <zzdzhangzzd@hotmail.com>
2019-01-23 15:18:40 +0800

    fix typoes of initial commit

--- fq.cpp
+++ fq.cpp
@@ -7,6 +7,7 @@
 #define BUFFER_SIZE 10000
 #include <string.h>
 #include <stdlib.h>
+#include "fq.h"
 
 static inline void next(const char * Id, const int Length, int &Last, int &Pos)
 {
@@ -24,7 +25,7 @@ static inline void next(const char * Id, const int Length, int &Last, int &Pos)
 void SeqId::resolve(const char * Id)
 {
 	int Last=0, Pos=0;
-	int Length=len(Id);
+	int Length=strlen(Id);
 	char temp[BUFFER_SIZE];
 	for (;Pos<Length;++Pos)
 	{
@@ -47,13 +48,13 @@ void SeqId::resolve(const char * Id)
 	next(Id,Length,Last,Pos);
 	tile=atoi(Id+Last);
 	next(Id,Length,Last,Pos);
-	x_pos=atoi(Id+Last)
+	x_pos=atoi(Id+Last);
 	next(Id,Length,Last,Pos);
 	y_pos=atoi(Id+Last);
 	next(Id,Length,Last,Pos);
 	read=atoi(Id+Last);
 	next(Id,Length,Last,Pos);
-	if_filtered=Id[Last];
+	is_filtered=Id[Last];
 	next(Id,Length,Last,Pos);
 	control_number=atoi(Id+Last);
 	next(Id,Length,Last,Pos);
